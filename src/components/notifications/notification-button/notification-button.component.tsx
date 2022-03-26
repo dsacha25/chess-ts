0e37d34b618432821f7c690c2066350a5d15dabc
David Sacha <dsacha25@gmail.com>
2022-03-25 19:01:38 -0700

    notification button state

--- src/components/notifications/notification-button/notification-button.component.tsx
+++ src/components/notifications/notification-button/notification-button.component.tsx
@@ -6,17 +6,18 @@ import {
 
 import { RiNotificationFill } from 'react-icons/ri';
 import NotificationsWindow from '../notifications-window/notifications-window.component';
-import { NotificationFlag } from '../notification-flag/notification-flag.styles';
 import { ClickAwayListener } from '@material-ui/core';
+import { useSelector } from '../../../hooks/use-selector/use-typed-selector.hook';
+import { selectUnreadNotifications } from '../../../redux/notifications/notifications.selector';
 
 const NotificationButton = () => {
-	const [hasUnreadNotifs, setHasUnreadNotifs] = useState(true);
+	const unreadNotifs = useSelector((state) => selectUnreadNotifications(state));
 	const [open, setOpen] = useState(false);
 
 	return (
 		<ClickAwayListener onClickAway={() => setOpen(false)}>
 			<NotificationButtonContainer onClick={() => setOpen(!open)} color="main">
-				<NotifButtonFlag unread={hasUnreadNotifs} />
+				<NotifButtonFlag unread={unreadNotifs.length > 0} />
 				<RiNotificationFill size="28px" />
 
 				{open && <NotificationsWindow />}
