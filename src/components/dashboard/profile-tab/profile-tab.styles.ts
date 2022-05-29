3074333cd08675d90fbab4afd65742d9b73c2101
David Sacha <dsacha25@gmail.com>
2022-05-28 17:55:38 -0700

    profile tab mobile layout

--- src/components/dashboard/profile-tab/profile-tab.styles.ts
+++ src/components/dashboard/profile-tab/profile-tab.styles.ts
@@ -17,8 +17,16 @@ export const ProfileContainer = styled.div`
 
 	@media screen and (max-width: 980px) {
 		display: flex;
+		width: 100vw;
+		overflow-y: auto;
 		flex-direction: column;
-		max-height: 100vh;
+		height: calc(100vh - 70px);
+		justify-content: flex-start;
+
+		h2 {
+			font-size: 30px;
+			margin: 10px;
+		}
 	}
 `;
 
@@ -38,12 +46,32 @@ export const UpdateProfileForm = styled.form`
 		justify-self: flex-start;
 		grid-column: 1 / span 2;
 	}
+
+	@media screen and (max-width: 980px) {
+		display: flex;
+		flex-direction: column;
+		width: 100vw;
+		height: 100vh;
+		grid-template: repeat(3, auto) / 1fr;
+		place-items: center;
+
+		h2 {
+			font-size: 16px;
+			margin: 10px;
+			grid-column: unset;
+			align-self: flex-start;
+		}
+	}
 `;
 
 export const ProfileDataContainer = styled.div`
 	display: grid;
 
 	place-items: center;
+
+	@media screen and (max-width: 980px) {
+		height: 150px;
+	}
 `;
 
 export const DeleteAccountButton = styled(CustomButton)`
@@ -58,6 +86,12 @@ export const SubmitUpdateButton = styled(CustomButton)`
 	width: 400px;
 	border-radius: 30px;
 	grid-column: 1 / span 2;
+
+	@media screen and (max-width: 980px) {
+		grid-column: unset;
+
+		width: 90vw;
+	}
 `;
 
 export const ReauthContainer = styled.div`
@@ -70,5 +104,14 @@ export const ReauthContainer = styled.div`
 
 	h2 {
 		justify-self: flex-start;
+	}
+
+	@media screen and (max-width: 980px) {
+		h2 {
+			font-size: 16px;
+			margin: 10px;
+			grid-column: unset;
+			align-self: flex-start;
+		}
 	}
 `;
