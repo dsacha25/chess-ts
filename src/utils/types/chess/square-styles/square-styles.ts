38e063aae5ed5da4f4b76550bf3708659f167913
David Sacha <dsacha25@gmail.com>
2022-08-09 15:30:14 -0700

    squarestyles CSSProperties update

--- src/utils/types/chess/square-styles/square-styles.ts
+++ src/utils/types/chess/square-styles/square-styles.ts
@@ -1,5 +1,5 @@
-import { CSSProperties } from '@material-ui/core/styles/withStyles';
 import { Square } from 'chess.js';
+import { CSSProperties } from 'styled-components';
 
 export type SquareStyles = {
 	[square in Square]?: CSSProperties;
